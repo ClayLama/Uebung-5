2e9ea9136bee92b657c1f0b4af6eedfa8b328a6f
Mattis Schreiner <whb398@haw-hamburg.de>
2024-06-06 22:36:18 +0200

    Kleine Verbesserungen

--- Aufgabe 2/EuroGeldscheinUeberpruefung.c
+++ Aufgabe 2/EuroGeldscheinUeberpruefung.c
@@ -18,22 +18,22 @@ int main() {
 	printf("*                                                    *\n");
 	printf("******************************************************\n\n");
 
+
 	eingabeSeriennummer(seriennummer);
 
 	printf("\nSeriennummer: ");
 	for (int i = 0; i < 2; i++)
 		printf("%c", seriennummer[i]);
 	for (int i = 2; i < 12; i++)
-		printf("%d", seriennummer[i]);
+		printf("%d\n", seriennummer[i]);
 
 	return 0;
 }
 
 int eingabeSeriennummer(int seriennummer[]) {
-	int korrekteEingabe = 0;
 	int anfangFuenfZiffern[5] = { 0 },
 		mitteVierZiffern[4] = { 0 },
-		letzteZiffer[1] = { 0 };
+		letzteZiffer[1] = { 0 };			//Die Bausteine der Seriennummer werden einzeln abgespeichert
 
 	//Eingabe der Seriennummer in 5 Stücken
 	printf("Im folgenden m\x81ssen Sie eine Seriennummer st%cckweise eingeben.\n\n", '\x81');
@@ -41,16 +41,16 @@ int eingabeSeriennummer(int seriennummer[]) {
 	printf("Bitte geben Sie den ersten Buchstaben ein.\n");
 	seriennummer[0] = eingabeBuchstabe();
 
-	printf("Bitte geben Sie den zweiten Buchstaben ein.\n");
+	printf("\nBitte geben Sie den zweiten Buchstaben ein.\n");
 	seriennummer[1] = eingabeBuchstabe();
 
-	printf("Bitte geben Sie die ersten fünf Ziffern ein.\n");
+	printf("\nBitte geben Sie die ersten fünf Ziffern ein.\n");
 	eingabeZahl(5, anfangFuenfZiffern);
 
-	printf("Bitte geben Sie die nachfolgenden vier Ziffern ein.\n");
+	printf("\nBitte geben Sie die nachfolgenden vier Ziffern ein.\n");
 	eingabeZahl(4, mitteVierZiffern);
 
-	printf("Bitte geben Sie die letzte Ziffer ein.\n");
+	printf("\nBitte geben Sie die letzte Ziffer ein.\n");
 	eingabeZahl(1, letzteZiffer);
 
 	//Zusammensetzung der Seriennummer
@@ -77,7 +77,8 @@ int eingabeBuchstabe() {
 		//Nur Buchstaben von a-z / A-Z
 		if ((eingabe > 90 || eingabe < 65) && (eingabe > 122 || eingabe < 97))
 			korrekteEingabe = 0;
-
+		
+		//Nur max ein Buchstabe
 		if (getchar() != '\n') {
 			korrekteEingabe = 0;
 			while (getchar() != '\n');
