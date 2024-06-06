02db52c7df06acb70e4c0f35d4e5176ea509fa29
Mattis Schreiner <whb398@haw-hamburg.de>
2024-06-06 22:22:14 +0200

    Eingabe der Zahlen fertig

--- Aufgabe 2/EuroGeldscheinUeberpruefung.c
+++ Aufgabe 2/EuroGeldscheinUeberpruefung.c
@@ -2,11 +2,15 @@
 //Programm zur Überprüfung eines Euro-Geldscheines anhand der Seriennummer
 #include <stdio.h>
 
-int eingabeSeriennummer();
+int eingabeSeriennummer(int seriennummer[]);
 
-char eingabeBuchstabe();
+int eingabeBuchstabe();
+
+int eingabeZahl(int anzahlZiffern, int ausgabeZiffern[]);
 
 int main() {
+	int seriennummer[12] = { 0 };
+
 	//Startnachricht
 	printf("******************************************************\n");
 	printf("*                                                    *\n");
@@ -14,23 +18,40 @@ int main() {
 	printf("*                                                    *\n");
 	printf("******************************************************\n\n");
 
-	eingabeSeriennummer();
+	eingabeSeriennummer(seriennummer);
 
 	return 0;
 }
 
-int eingabeSeriennummer() {
-	int seriennummer[12] = {0};
+int eingabeSeriennummer(int seriennummer[]) {
 	int korrekteEingabe = 0;
+	int anfangFuenfZiffern[5] = { 0 },
+		mitteVierZiffern[4] = { 0 },
+		letzteZiffer[1] = { 0 };
+
+	printf("Im folgenden m\x81ssen Sie eine Seriennummer st%cckweise eingeben.\n\n", '\x81');
 
 	printf("Bitte geben Sie den ersten Buchstaben ein.\n");
 	seriennummer[0] = eingabeBuchstabe();
+
+	printf("Bitte geben Sie den zweiten Buchstaben ein.\n");
+	seriennummer[1] = eingabeBuchstabe();
+
+	printf("Bitte geben Sie die ersten fünf Ziffern ein.\n");
+	eingabeZahl(5, anfangFuenfZiffern);
+
+	printf("Bitte geben Sie die nachfolgenden vier Ziffern ein.\n");
+	eingabeZahl(4, mitteVierZiffern);
+
+	printf("Bitte geben Sie die letzte Ziffer ein.\n");
+	eingabeZahl(1, letzteZiffer);
+
 	return 0;
 }
 
 
-char eingabeBuchstabe() {
-	char eingabe = 0;						//Eingegebene Zahl
+int eingabeBuchstabe() {
+	char eingabe = 0;						//Eingegebener Buchstabe 
 	int korrekteEingabe = 1;				//Wenn falsche Eingabe, dann = 0
 
 	do {
@@ -48,5 +69,45 @@ char eingabeBuchstabe() {
 		}
 	} while (!korrekteEingabe);
 
+	//Konvertierung zu Großbuuchstaben
+	if (eingabe >= 97)
+		eingabe = eingabe - 'a' + 'A';
+
 	return eingabe;
+}
+
+int eingabeZahl(int anzahlZiffern, int ausgabeZiffern[]) {
+	char eingabe[6] = { 0 };					//Eingegebener Zahl, einzeln als Ziffern gespeichert Ascii Buchstaben gespeichert
+	int korrekteEingabe = 1;					//Wenn falsche Eingabe, dann = 0
+
+	do {
+		if (korrekteEingabe)
+			printf("Zahl: ");
+		else
+			printf("Bitte geben Sie eine %d-stellige Zahl ein: ", anzahlZiffern);
+
+		korrekteEingabe = scanf_s("%s", &eingabe, 6);
+
+		//Überprüfung ob mehr als 5 Zahlen eingegeben
+		if (getchar() != '\n') {
+			korrekteEingabe = 0;
+			while (getchar() != '\n');
+		}
+
+		//Überprüfung, ob korrekte Anzahl an Ziffern
+			if (eingabe[anzahlZiffern] != '\0')
+				korrekteEingabe = 0;
+
+		//Überprüfung ob alles Zahlen und speichern als integerzahl
+		for (int i = 0; i < anzahlZiffern; i++) {
+			if (eingabe[i] < 48 || eingabe[i] > 57) {
+				korrekteEingabe = 0;
+				
+			}
+			ausgabeZiffern[i] = (int)eingabe[i] - 48;
+		}
+
+	} while (!korrekteEingabe);
+
+	return 0;
 }
