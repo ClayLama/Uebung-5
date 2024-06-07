ca1a27058cb489320fe9cc88737d6133b8571660
Mattis Schreiner <whb398@haw-hamburg.de>
2024-06-07 12:50:27 +0200

    Aufgabe 3 eingefügt

---
+++ Aufgabe 3/selectsort.c
@@ -0,0 +1,35 @@
+//Übung 4, Aufgabe 3: "EuroGeldscheinUeberpruefung(LK,MS).c" (07.06.2024) (Autoren:  Leif Kendelbacher, Mattis Schreiner)
+//Programm zur Sortierung von Zufallszahlen
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define LENGTH 10
+
+void selectsort(int n, int arr[]);
+
+int main() {
+	int i;
+	int arr[LENGTH];
+	srand(100);
+	printf("Vor dem Sortieren\n");
+	for (i = 0; i < LENGTH; i++)
+	{
+		arr[i] = rand();
+		printf("%6d", arr[i]);
+	}
+	printf("\n");
+	selectsort(LENGTH, arr);
+	printf("Nach dem Sortieren\n");
+	for (i = 0; i < LENGTH; i++)
+		printf("%6d", arr[i]);
+	printf("\n");
+	return 0;
+}
+
+void selectsort(int n, int arr[]) {
+	int arrKopie[LENGTH] = { 0 };
+	int minZahl = 0;
+
+	return;
+}
